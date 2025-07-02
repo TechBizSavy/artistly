368713a78c8336f5c6647bf345be5ea50c9639c5
Harsh <harshchalwadi027@gmail.com>
2025-07-03 00:36:11 +0530

    simple prac of aceternity ui

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "assets.aceternity.com",
+      "peerlist.io" 
+    ],
+  },
 };
 
 export default nextConfig;
